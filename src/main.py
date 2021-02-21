375bbeeff460640cc373d16b616308bd21660d5c
m-tanner <tanner.mbt@gmail.com>
2021-02-21 08:05:38 -0800

    fix margin, update main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import os
 
-from src.facebook_handler import FacebookHandler
 from src.spotify_handler import SpotifyHandler
 from src.config_manager import ConfigManager
 from src.fetcher_factory import FetcherFactory
@@ -13,17 +12,16 @@ def main(episode_name: str) -> None:
     config = ConfigManager()
 
     spotify_handler = SpotifyHandler(config=config)
-    spotify_handler.generate_per_feature_metrics()
+    spotify_handler.generate_per_feature_metrics()  # the songs have to be on the playlist for this to work
 
     fetcher = FetcherFactory(config=config).get_fetcher()
-    subscribers = fetcher.fetch_test_subscribers()
+    subscribers = fetcher.fetch_real_subscribers()
 
-    sender = Sender(
-        email_content=merge(
-            path_to_template="resources/template_email.html",
-            path_to_content=f"resources/episodes/{episode_name}.html",
-        )
+    email = merge(
+        path_to_template="resources/template_email.html",
+        path_to_content=f"resources/episodes/{episode_name}.html",
     )
+    sender = Sender(email_content=email)
     sender.send_to(subscribers=subscribers)
 
     merge(
@@ -40,12 +38,6 @@ def main(episode_name: str) -> None:
         path_to_file=os.path.join("resources/polished_episodes", f"{episode_name}.html")
     )
     uploader.upload_metrics(path_to_file="resources/metrics/metrics.json")
-
-    facebook = FacebookHandler(config=config)
-    facebook.post(
-        message="Today's episode is up!",
-        link=f"https://4trackfriday.com/show/episodes/{episode_name}.html"
-    )
 
 
 if __name__ == "__main__":
